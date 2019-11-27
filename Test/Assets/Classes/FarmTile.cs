78a139bf3df41c79f855c37d750ef6fb3ad56712
Pierce-James <jamespierce1412@gmail.com>
2019-11-27 18:49:22 +0000

    Initial hex grid setup

---
+++ Test/Assets/Classes/FarmTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmTile : MonoBehaviour
+{
+    bool irrigated;
+    Sprite m_sprite;
+    string cropType;
+    string status;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
