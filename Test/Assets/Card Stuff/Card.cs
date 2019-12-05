27753a69235d562b4679677b4ee6605f1a1d5bc0
Zachary <zachary.jacques160@gmail.com>
2019-12-05 16:27:38 -0500

    added temp sprites and anim for a tree, added scripts for deck and card system

---
+++ Test/Assets/Card Stuff/Card.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cards
+{
+    public enum Keyword
+    {
+        None,
+        Prune, 
+        Till,
+        Harvest,
+        Dig,
+        Test,
+        Seed,
+        Treat,
+        Fertilize,
+        Irrigation
+    };
+    public enum Position
+    {
+        None,
+        Deck,
+        Hand,
+        Discard
+    }
+
+    [CreateAssetMenu(fileName = "New Card", menuName = "Cards")]
+    public class Card : ScriptableObject
+    {
+        public string CardName;
+        public string CardDescription;
+
+        public Sprite Art;
+
+        public Position Location;
+        public Dictionary<Keyword, int> Mechanics;
+
+    }
+}
+
