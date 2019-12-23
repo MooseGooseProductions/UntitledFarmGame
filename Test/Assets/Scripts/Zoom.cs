1a377416de500343da26abe0735292c8e038caba
Pierce-James <jamespierce1412@gmail.com>
2019-12-23 19:20:14 +0000

    Merge

---
+++ Test/Assets/Scripts/Zoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Input.mousePosition, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                if (hit.collider.CompareTag("Tilemap"))
+                {
+                    Debug.Log("Hit detected!");
+                    this.gameObject.transform.Translate(new Vector3(0, 0, 300));
+                }
+            }
+            
+        }
+    }
+}
