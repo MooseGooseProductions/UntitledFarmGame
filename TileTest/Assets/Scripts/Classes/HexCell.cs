f3e47aefdfdd40f9b0cf64c376bbff2fa359fa89
Pierce-James <jamespierce1412@gmail.com>
2020-04-07 16:44:25 +0100

    Tile test project

---
+++ TileTest/Assets/Scripts/Classes/HexCell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell prefab;
+    HexCell[] cells;
+
+    void Awake()
+    {
+        cells = new HexCell[height * width];
+
+        for (int i = 0, j = 0; i < height; i++)
+        {
+            for (int k = 0; k < width; k++)
+            {
+                CreateCell(k, i, j++);
+            }
+        }
+    }
+
+    void CreateCell(int k, int i, int j)
+    {
+        Vector3 p;
+        p.x = k * 10f;
+        p.y = 0f;
+        p.z = i * 10f;
+
+        HexCell cell = cells[j] = Instantiate<HexCell>(prefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = p;
+    }
+}
---
+++ TileTest/Assets/Scripts/Classes/HexMetrics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class HexMetrics
+{
+    public const float rootThreeOverTwo = 0.86025404f;
+    public const float outRadius = 10f;
+    public const float innerRadius = outRadius * rootThreeOverTwo;
+
+    public static Vector3[] corners = {
+           new Vector3(0f, 0f, outRadius),
+           new Vector3(innerRadius, 0f, 0.5f * outRadius),
+           new Vector3(innerRadius, 0f, -0.5f * outRadius),
+           new Vector3(0f, 0f, -outRadius),
+           new Vector3(-innerRadius, 0f, -0.5f * outRadius),
+           new Vector3(-innerRadius, 0f, 0.5f * outRadius)
+    };
+}
